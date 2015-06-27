5dccf5195c554268cbb87eff4c6bee242209885c
Yos Riady <yosriady@gmail.com>
2015-06-27 17:25:36 +0800

    Use hex ids

--- app/controllers/gists_controller.rb
+++ app/controllers/gists_controller.rb
@@ -48,7 +48,7 @@ class GistsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_gist
-      @gist = Gist.find(params[:id])
+      @gist = Gist.find_by(hex: params[:hex])
       gon.gist = @gist
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :gists
+  resources :gists, param: :hex
 
   # Example resource route with options:
   #   resources :products do
